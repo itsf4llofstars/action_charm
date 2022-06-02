c16513af4690eaf756cc9889b43b32db520412de
itsf4llofstars <irooted4hal@mailfence.com>
2022-06-02 10:52:26 -0500

    Add function to strip off last newline char

--- file_ops.py
+++ file_ops.py
@@ -2,6 +2,13 @@
 
 
 def read_in_a_file(path: str):
+    """Reads in a file and returns a list of it's lines.
+
+    :param
+        path: [str]: Full path and filename
+    :return:
+        List[str]: List of the file with each index as a line
+    """
     read = 'r'
     try:
         with open(path, read) as fo:
@@ -12,9 +19,25 @@ def read_in_a_file(path: str):
         return input_file
 
 
+def strip_newlines(file_list):
+    """Strips off the last character in each list index. Works
+    if last character is a newline \n char.
+
+    :param
+        file_list: List[str]: List of strings
+    :return:
+        List[str]: List of strings stripped of the last character
+    """
+    stripped_list = []
+    for line in file_list:
+        stripped_list.append(line.strip())
+    return stripped_list
+
+
 def main() -> None:
     """main"""
-    file_text = read_in_a_file('/etc/bash.bashrc')
+    foo = read_in_a_file('/etc/adduser.conf')
+    file_text = strip_newlines(foo)
     [print(line) for line in file_text]
 
 
