ba0c5465856101019c07e76882042f52d96e436e
itsf4llofstars <irooted4hal@mailfence.com>
2022-06-02 10:40:32 -0500

    Add read in a file function
    Function reads a file, and returns a list with each line of the file as an index

---
+++ file_ops.py
@@ -0,0 +1,12 @@
+"""File Parser Python Script"""
+
+
+def read_in_a_file(path: str):
+    read = 'r'
+    try:
+        with open(path, read) as fo:
+            input_file = fo.readlines()
+    except FileNotFoundError as fnfe:
+        print(f'The file: {fnfe} was not found.')
+    else:
+        return input_file
