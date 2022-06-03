0960a480786601f15e3faa0baaa10e3d251d1c32
itsf4llofstars <irooted4hal@mailfence.com>
2022-06-03 15:54:35 -0500

    Add Calculate and print prime numbers

--- main.py
+++ main.py
@@ -10,6 +10,27 @@ def main():
     """main function"""
     max_primes = get_max_primes()
 
+    chk_prime = 1
+    if_prime = 2
+    primes = max_primes
+    total_primes = 0
+    iterations = 0
+
+    while primes > 0:
+        chk_prime += 1
+        iterations += 1
+
+        if if_prime % chk_prime == 0:
+            if if_prime == chk_prime:
+                primes -= 1
+                total_primes += 1
+                print(f"Prime: {total_primes} = {if_prime}")
+
+            chk_prime = 1
+            if_prime += 1
+
+    print(f"\nIterations: {iterations}")
+
 
 if __name__ == "__main__":
     import sys
