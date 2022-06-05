1b94314701dbe4f1804b56177dfb05831b3b7a72
itsf4llofstars <irooted4hal@mailfence.com>
2022-06-04 22:35:48 -0500

    Add hypot function

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 """main.py file"""
+import math
 
 
 def add_num(a: int, b: int) -> int:
     return a + b
 
 
+def hypot(a: float, b: float) -> float:
+    return math.sqrt(a ** 2 + b ** 2)
+
+
 def main():
     """main function"""
-    x: int = 33
-    y: int = 44
+    x: int = 30
+    y: int = 40
     z = add_num(x, y)
     print(f'{x} + {y} = {z}')
+
+    pythag = hypot(x, y)
+    print(pythag)
 
 
 if __name__ == "__main__":
