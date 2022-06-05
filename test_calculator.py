2f20ce76653fbc8d79daaff16cf5fe1130e33ab8
itsf4llofstars <irooted4hal@mailfence.com>
2022-06-04 22:41:45 -0500

    add test hypot function

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,5 @@
 from main import add_num
+from main import hypot
 
 
 def test_add_num() -> None:
@@ -6,3 +7,11 @@ def test_add_num() -> None:
     b = 123
     z = add_num(a, b)
     assert z == 360
+
+
+def test_hypot() -> None:
+    a = 300
+    b = 400
+    c = hypot(a, b)
+    assert c == 500
+
