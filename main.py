45ef7575bf1625a2d2d4872c46042fb0671dacee
itsf4llofstars <irooted4hal@mailfence.com>
2022-06-04 22:27:24 -0500

    Add add_num function

--- main.py
+++ main.py
@@ -2,9 +2,16 @@
 """main.py file"""
 
 
+def add_num(a: int, b: int) -> int:
+    return a + b
+
+
 def main():
     """main function"""
-    pass
+    x: int = 33
+    y: int = 44
+    z = add_num(x, y)
+    print(f'{x} + {y} = {z}')
 
 
 if __name__ == "__main__":
