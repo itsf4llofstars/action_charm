b67782b81a7e745291d32a5ac0e89d46fd59d57d
itsf4llofstars <irooted4hal@mailfence.com>
2022-06-06 19:07:17 -0500

    Add get_key method

--- cipher.py
+++ cipher.py
@@ -17,3 +17,15 @@ class Vigenere:
         self.key = ''
         self.message = ''
         self.encoded_message = ''
+
+    def get_key(self):
+        while True:
+            key = input("Enter a word to be used as the coding key: ")
+            if isinstance(key, str) and len(key) <= len(self.rotor_in):
+                self.key = key.upper()
+                break
+            else:
+                if not isinstance(key, str):
+                    print("\nPlease enter a word or letters form the 26 character North American Alphabet.\n")
+                elif len(key) > len(self.rotor_in):
+                    print("\nPlease keep your key to less than 26 characters.\n")
