b9db3b6eaea10b3d341d2485c4f53f0db73acae0
itsf4llofstars <irooted4hal@mailfence.com>
2022-06-06 19:10:16 -0500

    Set rotors to deques

--- cipher.py
+++ cipher.py
@@ -5,14 +5,14 @@ from collections import deque
 class Vigenere:
     def __init__(self):
         # fmt: off
-        self.rotor_in = [
+        self.rotor_in = deque([
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-        ]
-        self.rotor_in = [
+        ])
+        self.rotor_in = deque([
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-        ]
+        ])
         # fmt: on
         self.key = ''
         self.message = ''
