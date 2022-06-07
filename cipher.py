787582c29ca87829b8ff9a69b65a11cb20328ba2
itsf4llofstars <irooted4hal@mailfence.com>
2022-06-06 19:06:47 -0500

    Add Vigenere class

---
+++ cipher.py
@@ -0,0 +1,19 @@
+"""The Vigenere Cipher class file"""
+from collections import deque
+
+
+class Vigenere:
+    def __init__(self):
+        # fmt: off
+        self.rotor_in = [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+        ]
+        self.rotor_in = [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+        ]
+        # fmt: on
+        self.key = ''
+        self.message = ''
+        self.encoded_message = ''
